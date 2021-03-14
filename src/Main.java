b80df1c4de347508253fcc603c35ac258c199670
montsemsanz <montsemsanz@gmail.com>
2021-03-14 20:03:18 +0100

    Modificado println again

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("saludando desde Eclipse de nuevo");
+		System.out.println("saludando desde Eclipse again");
 
 	}
 
